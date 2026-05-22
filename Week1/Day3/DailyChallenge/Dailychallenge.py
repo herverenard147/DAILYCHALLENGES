f6672e60681810dea829f3db56ca1b0fa30c82ac
andrea <andreamariefrauchette@gmail.com>
2026-05-22 00:16:56 +0000

    first commiy

--- Week1/Day3/DailyChallenge/Dailychallenge.py
+++ Week1/Day3/DailyChallenge/Dailychallenge.py
@@ -1,26 +1,185 @@
-#Défi 1
-#Ask the user for a number and a length.
-#Create a program that prints a list of multiples of the number until the list length reaches length.
+# ==========================================
+# Création de la classe Farm
+# ==========================================
+
+class Farm:
+
+    # ==========================================
+    # Constructeur
+    # ==========================================
+    def __init__(self, farm_name):
+
+        # Nom de la ferme
+        self.name = farm_name
+
+        # Dictionnaire vide pour stocker les animaux
+        # Exemple :
+        # {
+        #   "cow": 5,
+        #   "sheep": 2
+        # }
+        self.animals = {}
 
 
-number=int(input("Entrez un nombre :   "  ))
-length=int(input("Entrez une longueur :   "  ))
-multiples=[]
-for i in range(1,length+1):
-    multiples.append(number*i)
-print(multiples)
+    # ==========================================
+    # Méthode pour ajouter des animaux
+    # ==========================================
+    def add_animal(self, animal_type=None, count=1, **kwargs):
+
+        # ------------------------------------------
+        # Cas 1 : ajout classique
+        # Exemple :
+        # add_animal("cow", 5)
+        # ------------------------------------------
+        if animal_type:
+
+            # Vérifie si l'animal existe déjà
+            if animal_type in self.animals:
+
+                # Ajouter la quantité
+                self.animals[animal_type] += count
+
+            else:
+                # Créer une nouvelle entrée
+                self.animals[animal_type] = count
 
 
+        # ------------------------------------------
+        # Cas 2 : ajout multiple avec **kwargs
+        # Exemple :
+        # add_animal(cow=5, sheep=2, goat=12)
+        # ------------------------------------------
+        for animal, qty in kwargs.items():
 
-#Défi 2
-#Write a program that asks a string to the user, and display a new string with any duplicate consecutive letters removed.
+            # Vérifier si l'animal existe déjà
+            if animal in self.animals:
+
+                # Ajouter la quantité
+                self.animals[animal] += qty
+
+            else:
+                # Ajouter l'animal
+                self.animals[animal] = qty
 
 
-word = input("Entrez un mot : ")
+    # ==========================================
+    # Méthode pour afficher les informations
+    # ==========================================
+    def get_info(self):
 
-result = ""
-for i in range(len(word)):
-    if i == 0 or word[i] != word[i-1]:
-        result += word[i]
+        # Début du texte
+        info = f"{self.name}'s farm\n\n"
 
-print(result)
+        # Parcourir les animaux
+        for animal, count in self.animals.items():
+
+            # Ajouter chaque animal au texte
+            info += f"{animal:<10} : {count}\n"
+
+        # Ajouter la phrase finale
+        info += "\n\tE-I-E-I-0!"
+
+        # Retourner le texte
+        return info
+
+
+    # ==========================================
+    # Méthode pour retourner les types d'animaux
+    # ==========================================
+    def get_animal_types(self):
+
+        # Retourner les clés triées
+        return sorted(self.animals.keys())
+
+
+    # ==========================================
+    # Méthode courte d'information
+    # ==========================================
+    def get_short_info(self):
+
+        # Liste des animaux triés
+        animal_list = []
+
+        # Parcourir les animaux
+        for animal in self.get_animal_types():
+
+            # Ajouter "s" si le nombre > 1
+            if self.animals[animal] > 1:
+                animal_list.append(animal + "s")
+
+            else:
+                animal_list.append(animal)
+
+        # Construire la phrase finale
+        sentence = ", ".join(animal_list[:-1])
+
+        # Ajouter le dernier animal avec "and"
+        if len(animal_list) > 1:
+            sentence += " and " + animal_list[-1]
+
+        elif len(animal_list) == 1:
+            sentence = animal_list[0]
+
+        # Retourner la phrase
+        return f"{self.name}'s farm has {sentence}."
+
+
+# ==========================================
+# TEST DU PROGRAMME
+# ==========================================
+
+# Création de la ferme
+macdonald = Farm("McDonald")
+
+
+# ==========================================
+# Ajouter des animaux (méthode classique)
+# ==========================================
+
+macdonald.add_animal('cow', 5)
+
+# sheep sans quantité = 1 par défaut
+macdonald.add_animal('sheep')
+
+# Ajouter encore un sheep
+macdonald.add_animal('sheep')
+
+# Ajouter des goats
+macdonald.add_animal('goat', 12)
+
+
+# ==========================================
+# Affichage des informations
+# ==========================================
+
+print(macdonald.get_info())
+
+
+# ==========================================
+# Affichage des types d'animaux
+# ==========================================
+
+print("\nListe triée des animaux :")
+print(macdonald.get_animal_types())
+
+
+# ==========================================
+# Affichage version courte
+# ==========================================
+
+print("\nVersion courte :")
+print(macdonald.get_short_info())
+
+
+# ==========================================
+# BONUS : Ajouter plusieurs animaux
+# ==========================================
+
+macdonald.add_animal(
+    horse=3,
+    chicken=7,
+    duck=4
+)
+
+print("\n=== Après ajout multiple ===")
+print(macdonald.get_info())
--- Week1/Day3/ExerciseXP/ExerciceXP.py
+++ Week1/Day3/ExerciseXP/ExerciceXP.py
@@ -1,106 +1,141 @@
-# #Instructions
-# # Challenge 1
-# #Imprimez le résultat suivant en utilisant une seule ligne de code :
-# #Hello world
-# #Hello world
-# #Hello world
-# ##Hello world
+# Création de la classe Zoo
+class Zoo:
+
+    # Constructeur
+    def __init__(self, zoo_name):
+
+        # Nom du zoo
+        self.zoo_name = zoo_name
+
+        # Liste vide pour stocker les animaux
+        self.animals = []
+
+        # Dictionnaire pour les groupes
+        self.groups = {}
 
 
+    # =========================
+    # Ajouter un ou plusieurs animaux
+    # =========================
+    def add_animal(self, *new_animals):
 
-from ast import If
+        # Boucle sur tous les animaux reçus
+        for animal in new_animals:
 
+            # Vérifier si l'animal existe déjà
+            if animal not in self.animals:
 
-print("Hello world:\n"*3)
+                # Ajouter l'animal
+                self.animals.append(animal)
 
-
-
-
-# # Challenge 2
-# #Exercice 2
-#Write code that calculates the result of:
-#(99^3)*8 (meaning 99 to the power of 3, times 8).
-resultat=(99**3)*8
-print(resultat)
-
-
-
-# Exercice 3
-#Instructions
-#Predict the output of the following code snippets:
-#Coment what is your guess, then run the code and compare
+            else:
+                print(f"{animal} existe déjà dans le zoo.")
 
 
-print(5 < 3) #False car 5 est plus grand que 3
-print(3 == 3) #True car les deux sont égaux
-print(3 == "3") # TypeError impossible de comparer str et int avec 
-print("3" > 3) #False car les types sont différents (str et int)
-print("Hello" == "hello") #False car les chaînes de caractères sont sensibles à la casse            
+    # =========================
+    # Afficher les animaux
+    # =========================
+    def get_animals(self):
+
+        print(f"\nAnimaux du zoo {self.zoo_name} :")
+
+        for animal in self.animals:
+            print(animal)
 
 
-#Exercice 4
-#Instructions
-#Create a variable called computer_brand which value is the brand name of your computer.
-#Using the computer_brand variable, print a sentence that states the following:
-#"I have a <computer_brand> computer."
+    # =========================
+    # Vendre un animal
+    # =========================
+    def sell_animal(self, animal_sold):
 
-computer_brand="Hp"
-print(f"I have a {computer_brand} computer.")
+        # Vérifier si l'animal existe
+        if animal_sold in self.animals:
 
-#Exercice 5
-#Instructions
-#Instructions
-#Create a variable called name, and set it’s value to your name.
-#Create a variable called age, and set it’s value to your age.
-#Create a variable called shoe_size, and set it’s value to your shoe size.
-#Create a variable called info and set it’s value to an interesting sentence about yourself. The sentence must contain all the variables created in parts 1, 2, and 3.
-#Have your code print the info message.
-#Run your code.
+            # Supprimer l'animal
+            self.animals.remove(animal_sold)
+
+            print(f"{animal_sold} a été vendu.")
+
+        else:
+            print(f"{animal_sold} n'existe pas dans le zoo.")
 
 
-name="Lylda"
-age=21
-shoe_size=38
-info=f"My name is {name}, I am {age} years old and my shoe size is {shoe_size}."
-print(info)
+    # =========================
+    # Trier et regrouper les animaux
+    # =========================
+    def sort_animals(self):
 
-##Exercise 6
-#Instructions
-#Create two variables, a and b.
-#Each variable’s value should be a number.
-#If a is bigger than b, have your code print "Hello World".
-a=15
-b=7
-if a>b :
-    print("Hello world")
+        # Trier la liste alphabétiquement
+        sorted_animals = sorted(self.animals)
+
+        # Dictionnaire vide
+        grouped = {}
+
+        # Parcourir les animaux triés
+        for animal in sorted_animals:
+
+            # Première lettre
+            first_letter = animal[0]
+
+            # Si la lettre n'existe pas encore
+            if first_letter not in grouped:
+
+                # Créer une nouvelle liste
+                grouped[first_letter] = []
+
+            # Ajouter l'animal dans le groupe
+            grouped[first_letter].append(animal)
+
+        # Sauvegarder dans l'attribut groups
+        self.groups = grouped
+
+        return grouped
 
 
-#Exercice 7
-#Instructions
-#Write code that asks the user for a number and determines whether this number is odd or even.
+    # =========================
+    # Afficher les groupes
+    # =========================
+    def get_groups(self):
 
-nombre=input("Entrez un nombre :   "  )
-if (int(nombre)%2 ==0):
-    print("Le nombre est pair")
-else:    print("Le nombre est impair")
+        print("\nGroupes d'animaux :")
+
+        for letter, animals in self.groups.items():
+            print(f"{letter}: {animals}")
 
 
-#Exercice 8
-#Instructions
-#Write code that asks the user for their name and determines whether or not you have the same name. Print out a funny message based on the outcome.
-nom1=input("Entrez votre nom :   "  )
-nom2="Lylda"            
-if nom1==nom2:
-    print("You , c'est le même nom!")   
+# =========================
+# Étape 2 : Création du zoo
+# =========================
 
-else:    print("Oups, les noms sont différents, reessayez !")    
+brooklyn_safari = Zoo("Brooklyn Safari")
 
-#Exercise 9
-#Write code that will ask the user for their height in centimeters.
-#If they are over 145 cm, print a message that states they are tall enough to ride.
-#If they are not tall enough, print a message that says they need to grow some more to ride.
 
-taille=int(input("Entrez votre taille en cm :   "  ))
-if taille>145:
-    print("Vous êtes assez grand pour faire du manège !")   
-else:    print("Vous devez grandir un peu plus pour faire du manège !")
+# =========================
+# Étape 3 : Utilisation des méthodes
+# =========================
+
+# Ajouter des animaux
+brooklyn_safari.add_animal(
+    "Giraffe",
+    "Bear",
+    "Baboon",
+    "Lion",
+    "Zebra",
+    "Cat",
+    "Cougar"
+)
+
+# Afficher les animaux
+brooklyn_safari.get_animals()
+
+# Vendre un animal
+brooklyn_safari.sell_animal("Bear")
+
+# Réafficher les animaux
+brooklyn_safari.get_animals()
+
+# Trier et regrouper
+brooklyn_safari.sort_animals()
+
+# Afficher les groupes
+brooklyn_safari.get_groups()
